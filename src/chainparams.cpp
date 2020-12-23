174445a74d01ddda81c8fd6e48977b531f1aa17d
motty <admin@fujicoin.org>
2020-12-23 10:50:21 +0900

    fix error

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -280,7 +280,7 @@ public:
         vSeeds.clear();
 
         if (!args.IsArgSet("-signetchallenge")) {
-            bin = ParseHex(signet_challenge[0]);
+            bin = ParseHex("512103ad5e0edad18cb1f0fc0d28a3d4f1f3e445640337489abb10404f2d1e086be430210359ef5021964fe22d6f8e05b2463c9540ce96883fe3b278760f048f5189f2e6c452ae");
 
             consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000100010");
             consensus.defaultAssumeValid = uint256S("3101fe06ff57ecec2fbb8d70327e6fa4faaeff18cacda8f26e46395c6c94b4f1"); // 0
